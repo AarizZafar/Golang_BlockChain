2b8f38d6c6935e0231f26c6817dbaec59c18ca3a
AarizZafar <95307132+AarizZafar@users.noreply.github.com>
2024-07-25 14:04:07 +0530

    wallet_server created

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -14,6 +14,9 @@ import (
    once its made we want to tstore it in out cache 
  */
 var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
+/* Creates a variable named cache that is a map (key value pair) 
+key - string 
+values (is a pointer)- block.Blockchain */
 
 type BlockchainServer struct {
     port uint16
@@ -26,6 +29,7 @@ func NewBlockChainServer(port uint16) *BlockchainServer {
 func (bcs *BlockchainServer) Port() uint16 {
     return bcs.port
 }
+
 
 func (bcs *BlockchainServer) GetBlockchain() *block.Blockchain {
     bc, ok := cache["blockchain"]  // checking if we have the blockchain in our cache or not
